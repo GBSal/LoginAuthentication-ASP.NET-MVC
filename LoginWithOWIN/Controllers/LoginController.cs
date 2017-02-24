1bf3b16cf995c353fa381fcd39055acff134504c
Mohammad Salman <msalman@macmillan.org.uk>
2017-02-24 18:55:18 +0000

    Add project files.

---
+++ LoginWithOWIN/Controllers/LoginController.cs
@@ -0,0 +1,77 @@
+﻿using LoginWithOWIN.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Owin.Security;
+
+namespace LoginWithOWIN.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        UserSerice _userServicve;
+        public LoginController()
+        {
+
+        }
+
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+
+
+
+
+                return RedirectToRoute(new { Controller = "Home", Action = "Index" });
+            }
+
+
+            return View("Index");
+
+        }
+
+
+        private void AuthenticateUser(UserModel user , bool isPersistence =false)
+        {
+
+            var claims = new[] {
+                new Claim(ClaimTypes.Name, user.Name),
+                new Claim(ClaimTypes.Email, user.Email)
+            };
+
+            var identity = new ClaimsIdentity(claims, DefaultApplicationTypes.ApplicationCookie);
+
+            var roles = _roleService.GetByUserId(user.Id).ToList();
+            if (roles.Any())
+            {
+                var roleClaims = roles.Select(r => new Claim(ClaimTypes.Role, r.Name));
+                identity.AddClaims(roleClaims);
+            }
+
+            var context = Request.GetOwinContext();
+            var authManager = context.Authentication;
+
+            authManager.SignIn(new AuthenticationProperties { IsPersistent = isPersistence }, identity);
+        }
+
+        private class UserSerice
+        {
+        }
+    }
+
+    }
+}
---
+++ LoginWithOWIN/Services/UserService.cs
@@ -0,0 +1,40 @@
+﻿using LoginWithOWIN.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LoginWithOWIN.Services
+{
+    public class UserService
+    {
+
+
+        public UserModel GetByUserId(int Id)
+        {
+
+            var user = GetUsers()
+                        .Where(x => x.Id == Id)
+                        .SingleOrDefault();
+
+            return user;
+
+        }
+
+
+        private List<UserModel> GetUsers()
+        {
+
+            return new List<UserModel> {
+                new UserModel { Id = 1, Email = "john@mail.com", Name = "John", Password = "john" },
+                new UserModel { Id = 2, Email = "chris@mail.com", Name = "Chris", Password = "chris" },
+                new UserModel { Id = 3, Email = "sam@mail.com", Name = "Sam", Password = "sam" },
+                new UserModel { Id = 4, Email = "ellie@hotmail.com", Name = "Ellie", Password = "ellie" },
+        };
+
+        }
+
+
+
+    }
+}
